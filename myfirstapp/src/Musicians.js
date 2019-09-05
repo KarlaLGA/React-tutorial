00e6def1988ff60467533bc29dc381087df072aa
KarlaLGA <KarlaLGA@users.noreply.github.com>
2019-09-04 20:23:33 -0700

    Update Musicians component to a stateless component

--- myfirstapp/src/Musicians.js
+++ myfirstapp/src/Musicians.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Musicians extends Component {
-    render(){
-        console.log(this.props);
-        const { musicians } = this.props;
-        const musicianList = musicians.map( musician => {
-            return (
-                <div className="musician" key={musician.id}>
-                    <div>Name: {musician.name}</div>
-                    <div>Age: {musician.age}</div>
-                    <div>Genre: {musician.genre}</div>
-                </div>
-            )
-        })
+const Musicians = ({musicians}) => {
+    console.log(musicians);
+    const musicianList = musicians.map( musician => {
         return (
-            <div className="musician-list">
-                {musicianList}
+            <div className="musician" key={musician.id}>
+                <div>Name: {musician.name}</div>
+                <div>Age: {musician.age}</div>
+                <div>Genre: {musician.genre}</div>
+                <br/>
             </div>
         )
-    }
+    })
+    return (
+        <div className="musician-list">
+            {musicianList}
+        </div>
+    )
 }
 
 export default Musicians
