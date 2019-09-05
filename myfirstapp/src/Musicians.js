e8857b9fcb26f2f013f258926815ce9b925ea03e
KarlaLGA <KarlaLGA@users.noreply.github.com>
2019-09-04 22:22:32 -0700

    Add conditionals

--- myfirstapp/src/Musicians.js
+++ myfirstapp/src/Musicians.js
@@ -2,21 +2,69 @@ import React from 'react';
 
 const Musicians = ({musicians}) => {
     console.log(musicians);
-    const musicianList = musicians.map( musician => {
-        return (
-            <div className="musician" key={musician.id}>
+    // CONDITION USING IF STATEMENT
+
+    // const musicianList = musicians.map( musician => {
+    //     if (musician.age > 30 ){
+
+    //         return (
+    //             <div className="musician" key={musician.id}>
+    //                 <div>Name: {musician.name}</div>
+    //                 <div>Age: {musician.age}</div>
+    //                 <div>Genre: {musician.genre}</div>
+    //                 <br/>
+    //             </div>
+    //         )
+    //     }
+    //     else {
+    //         return null
+    //     }
+    // });
+
+    // return (
+    //     <div className="musician-list">
+    //         {musicianList}
+    //     </div>
+    // )
+
+
+    // CONDITION USING TERNIARY OPERATOR
+
+    const musicianList = musicians.map ( musician => {
+        return musician.age > 30 ? (
+             <div className="musician" key={musician.id}>
                 <div>Name: {musician.name}</div>
                 <div>Age: {musician.age}</div>
                 <div>Genre: {musician.genre}</div>
                 <br/>
             </div>
-        )
-    })
+        ) : null
+    });
+    
     return (
         <div className="musician-list">
             {musicianList}
         </div>
     )
+
+
+    // CONDITION INSIDE RETURN TEMPLATE
+
+    // return (
+    //    <div className='musician-list'>
+    //        {
+    //            musicians.map(musician => {
+    //                return musician.age > 30 ? (
+    //                <div className='musician' key={musician.id}>
+    //                     <div>Name: { musician.name } </div>
+    //                     <div>Age: { musician.age } </div>
+    //                     <div>Genre: { musician.genre } </div>
+    //                </div>
+    //                ) : null;
+    //            })
+    //        }
+    //    </div> 
+    // )
 }
 
 export default Musicians
