1a51662d0c04d2207aa6572ea73b7072e9d15763
KarlaLGA <KarlaLGA@users.noreply.github.com>
2019-09-18 17:31:29 -0700

    Create new app
    Working with React Router

---
+++ poketime/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, velit maiores reprehenderit, architecto quibusdam rem enim ad magni tempora dolor libero voluptatum dignissimos, repellendus veritatis itaque odio provident sit iusto.</p>
+        </div>
+    )
+}
+
+export default About
---
+++ poketime/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, velit maiores reprehenderit, architecto quibusdam rem enim ad magni tempora dolor libero voluptatum dignissimos, repellendus veritatis itaque odio provident sit iusto.</p>
+        </div>
+    )
+}
+
+export default Home
