0fded4932c865cde3872e663a5406c3021a7c7f1
KarlaLGA <KarlaLGA@users.noreply.github.com>
2019-09-25 17:41:37 -0700

    Add route parameters and images

--- poketime/src/components/Home.js
+++ poketime/src/components/Home.js
@@ -1,5 +1,7 @@
 import React, { Component} from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+import Pokeball from '../pokeball.png';
 
 class Home extends Component {
     state = {
@@ -24,9 +26,12 @@ class Home extends Component {
         const postList = posts.length ? (
             posts.map(post => {
                 return (
-                    <div className="post card" key={post.id}>
+                    <div className="post card" key={post.id} id={post.id}>
+                        <img src={Pokeball} alt="A pokeball"/>
                         <div className="card-content">
-                            <span className="card-title">{post.title}</span>
+                            <Link to={`/${post.id}`}> 
+                                <span className="card-title red-text">{post.title}</span>
+                            </Link>
                             <p>{post.body}</p>
                         </div>
                     </div>
@@ -36,7 +41,7 @@ class Home extends Component {
             <div className="center">No posts yet</div>
         )
         return (
-            <div className="container">
+            <div className="container home">
                 <h4 className="center">Home</h4>
                 {postList}
             </div>
